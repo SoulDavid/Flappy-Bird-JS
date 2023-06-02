c2757c22fcd6c2909b957ee541b549a35e0e39f9
David Martín Almazán <44950611+SoulDavid@users.noreply.github.com>
2023-06-02 13:55:15 +0200

    Update background.js

--- code/background.js
+++ code/background.js
@@ -1,24 +1,35 @@
+//Clase Background que recoge sus propiedades como el body y la imagen
 class Background 
 {
+    //Constructor por defecto
     constructor(position)
     {
+        //Propiedad del cuerpo
         this.body = null;
+        //Propiedad de la imagen
         this.image = graphicAssets.background.image;
     }
 
+    //Propiedad para pintar por canvas el background
     Draw(ctx)
     {
         ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height);
     }
 }
 
-class Background_game{
+//Clase Background del juego que recoge sus propiedades como el body y la imagen
+class Background_game
+{
+    //Constructor por defecto
     constructor(position)
     {
+        //Propiedad del cuerpo
         this.body = null;
+        //Propiedad de la imagen
         this.image = graphicAssets.background_game.image;
     }
-
+    
+    //Propiedad para pintar por canvas el background
     Draw(ctx)
     {
         ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height);
